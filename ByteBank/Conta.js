9bb05c76bc161421dec66702e8afad31f0ab8d3c
Daniel Batista <danielbped@gmail.com>
2021-02-16 17:40:02 -0300

    Versão 1.11

--- ByteBank/Conta.js
+++ ByteBank/Conta.js
@@ -1,13 +1,14 @@
+//Classe abstrata (Apenas pode ser herdada)
 export class Conta {
     constructor(saldoInicial, cliente, agencia) {
+        if(this.constructor == Conta){
+            throw new Error("Você não deveria instanciar um objeto do tipo Conta diretamente")
+        }
+
         this._saldo = saldoInicial;
         this._cliente = cliente;
         this._agencia = agencia;
-       
-        if(this.constructor == Conta){
-            console.log("Você não deveria instanciar um objeto do tipo Conta")
-        }
-       
+
     }
 
     set cliente(novoValor) {
--- ByteBank/index.js
+++ ByteBank/index.js
@@ -3,7 +3,7 @@ import { Conta } from "./Conta.js";
 import {ContaCorrente} from "./ContaCorrente.js";
 import { ContaPoupanca } from "./ContaPoupanca.js";
 
-const cliente1 = new Cliente("Ricardo", 11122233309);
+const cliente1 = new Cliente("Daniel", 11122233309);
 const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
 const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-//const conta = new Conta(0, cliente1, 1001);
+const conta = new Conta(0, cliente1, 1001);
