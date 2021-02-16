f5ae815ae2ff01e065b76e663930ac1bb476eadf
Daniel Batista <danielbped@gmail.com>
2021-02-16 18:42:02 -0300

    Versão 1.2
    Fim da aula de Interfaces e Herança em Orientação a Objetos

--- ByteBank/Contas/Conta.js
+++ ByteBank/Contas/Conta.js
@@ -25,9 +25,9 @@ export class Conta {
         return this._saldo;
     }
 
+    //Método abstrato
     sacar(valor) {
-        let taxa = 1
-        return this._sacar(valor, taxa);
+        throw new Error("O método Sacar da conta é abstrato")
     }
 
     _sacar(valor, taxa){
--- ByteBank/index.js
+++ ByteBank/index.js
@@ -1,9 +1,18 @@
 import {Cliente} from "./Cliente.js";
-import { Conta } from "./Conta.js";
-import {ContaCorrente} from "./ContaCorrente.js";
-import { ContaPoupanca } from "./ContaPoupanca.js";
+import {Gerente} from "./Funcionários/Gerente.js"
+import {Diretor} from "./Funcionários/Diretor.js"
+import {SistemaAutenticacao} from "./SistemaAutenticacao.js"
 
-const cliente1 = new Cliente("Daniel", 11122233309);
-const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
-const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
-const conta = new Conta(0, cliente1, 1001);
+const diretor = new Diretor("Rodrigo", 10000, 1231456780);
+diretor.cadastrarSenha("123456789")
+const gerente = new Gerente("Rodolfo", 5000, 12378945601);
+gerente.cadastrarSenha("123456")
+
+const cliente = new Cliente("Daniel", 789456142375, "456");
+const estaLogadoDiretor = SistemaAutenticacao.login(diretor, "123456789");
+const estaLogadoGerente = SistemaAutenticacao.login(gerente, "123456");
+const estaLogadoCliente = SistemaAutenticacao.login(cliente, "456");
+
+console.log(estaLogadoDiretor);
+console.log(estaLogadoGerente);
+console.log(estaLogadoCliente);
