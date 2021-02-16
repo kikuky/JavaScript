60bad3631ba6ccab4699a45af94cd218e185b04f
Daniel Batista <danielbped@gmail.com>
2021-02-16 17:13:36 -0300

    Versao 1.1

---
+++ ByteBank/index.js
@@ -0,0 +1,9 @@
+import {Cliente} from "./Cliente.js";
+import { Conta } from "./Conta.js";
+import {ContaCorrente} from "./ContaCorrente.js";
+import { ContaPoupanca } from "./ContaPoupanca.js";
+
+const cliente1 = new Cliente("Ricardo", 11122233309);
+const contaCorrenteRicardo = new ContaCorrente( cliente1, 1001);
+const contaPoupanca = new ContaPoupanca(50, cliente1, 1001);
+//const conta = new Conta(0, cliente1, 1001);
